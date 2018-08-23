3531b2aa6078cc3dc7e677453622b7fab1966e0c
Shawn <shawnlim0814@gmail.com>
2018-08-23 14:24:03 +0800

    raymond

---
+++ RS/restore.php
@@ -0,0 +1,28 @@
+<?php 
+include'_config.php';
+echo "<link rel='stylesheet' href='/css/footer.css'>";
+
+if ($page->is_post()) {
+    $sql = file_get_contents('product.sql');
+    $pdo = new PDO('mysql:host=localhost;port=3306', 'root', '');
+    $pdo->exec($sql);
+    
+    $page->temp('success', 'Database restored.');
+}
+$page->title='Registration';
+$page->header();
+?>
+
+<p class="success"><?= $page->temp('success') ?></p>
+
+<p>Start MySQL and click the <b>Restore Database</b> button.<p>
+
+<form method="post">
+    <button>Restore Database</button>
+</form>
+
+
+
+<?php 
+$page->footer();
+?>
