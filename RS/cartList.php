259e5b1dea0a5150a12967363852ea9124659731
Shawn <shawnlim0814@gmail.com>
2018-08-23 14:33:09 +0800

    raymond

--- RS/cartList.php
+++ RS/cartList.php
@@ -3,23 +3,34 @@ include'_config.php';
 $page->title='Cart List';
 $page->header();
 echo "<link rel='stylesheet' href='/css/footer.css'>";
+echo "<link rel='stylesheet' href='/css/cartList.css'>";
 ?>
 
+// Get the product list from the data base
+<h1>Cart List</h1>
+<table class="cartList"> 
+<tr>
+    <th id="item">Item</th>
+    <th id="price">Product Price</th>
+    <th quatity="quantity">Quantity</th>
+    <th id="tPrice">Total Price</th>
+    <th id="order">Order</th>
+</tr>
+<tr>
+    <td>asdj</td>
+    <td>lkflg</td>
+    <td>df</td>
+    <td id="order">sdjf</td>
+</tr>
 
-<form>
-    <h1>Cart List</h1>
-    <table> 
-    <tr>
-        <th>Item</th>
-        <th>Product Price</th>
-         <th>Total Price</th>
-        <th>Order</th>
-    </tr>
-    <tr>
-        <td></td>
-    </tr>
-    </table>
-</form>
+<tr>
+    <td>asdj</td>
+    <td>lkflg</td>
+    <td>df</td>
+    <td>sdjf</td>
+</tr>
+</table>
+
 
 
 
