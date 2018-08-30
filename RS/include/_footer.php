69a6753b47c7eb8e83d38289b80a082e18ed800f
Ashion <benlyr1212@gmail.com>
2018-08-30 10:53:02 +0800

    modify footer.php code

--- RS/include/_footer.php
+++ RS/include/_footer.php
@@ -1,15 +1,13 @@
 <footer>
     <div id="footer-container" >
         <div id="left_footer">
-            <a href="/orderList.php">Order List</a>
             <a href='/aboutUs.php'>About Us</a>
         </div>
 
     </div>
-    <div id="address"> Malaysia &copy<?=date("Y"); ?> RS, Inc. All Right Reserved</div>
+    <div id="address"> Malaysia &copy<?= date("Y"); ?> RS, Inc. All Right Reserved</div>
 </footer>
-           
-    </body>
-    
-  
+
+</body>
+
 </html>
